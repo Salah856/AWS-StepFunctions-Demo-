b3338651261a6d4a4517dba46b3a37cb744809a9
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-21 08:33:40 +0200

    Create process_purchase_lambda.py

---
+++ process_purchase_lambda.py
@@ -0,0 +1,13 @@
+
+def lambda_handler(event, context):
+    
+    print('event:', event)
+
+    response = {
+        'TransactionType': event['TransactionType'],
+        'Message': 'From Process Purchase',
+    }
+
+    return response
+  
+  
