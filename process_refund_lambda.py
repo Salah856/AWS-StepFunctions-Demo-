d0a31d3d62b2963075dea88afd49a88dd1689abc
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-21 08:34:21 +0200

    Create process_refund_lambda.py

---
+++ process_refund_lambda.py
@@ -0,0 +1,14 @@
+
+def lambda_handler(event, context):
+    print('event:', event)
+
+    response = {
+        'TransactionType': event['TransactionType'],
+        'Message': 'From Process Refund',
+    }
+
+    return response
+  
+  
+  
+  
